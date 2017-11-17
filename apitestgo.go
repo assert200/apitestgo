2d7bcb58d862219bfac581f400ffc5a2deba3dc2
James Barker <james.barker@cultureamp.com>
2017-11-17 15:01:05 +1100

    update example

--- apitestgo.go
+++ apitestgo.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	request := gorest.NewRequest()
+	request := gorest.NewRestRequest()
 
 	var url url.URL
 	url.Scheme = "https"
@@ -18,17 +18,12 @@ func main() {
 	request.Method = "GET"
 	request.URL = url
 
-	session := gorest.NewSession()
+	test := gorest.RestTest{}
+	test.Description = "google"
+	test.RestRequest = request
+	test.ExpectedStatusCode = 200
 
-	response, err := gorest.Do(session, request)
+	doneTest := gorest.DoAndVerify(test)
 
-	if err != nil {
-		log.Fatal("There was an error excuting the rest request: " + err.Error())
-	}
-
-	if response.StatusCode != 200 {
-		log.Fatal("Expecting Status Code 200, Recieved: ", response.StatusCode)
-	}
-
-	log.Println("PASS: Response Status Code: ", response.StatusCode)
+	log.Println(doneTest.Description, doneTest.RestResponse.StatusCode)
 }
